d850451ee40654710b00e485d3a95011b880ee1a
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2026-04-13 19:15:17 +0530

    added component

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,10 +1,12 @@
 import { fetchPhotos, fetchVideo } from "./API/mediaApi"
 import { SearchBar } from "./Component/SearchBar"
+import { Tabs } from "./Component/Tabs"
 const App = () => {
 
   return (
     <div className="bg-black h-screen w-full">
       < SearchBar />
+      < Tabs />
     </div>
   )
 }
---
+++ project/src/Component/Tabs.jsx
@@ -0,0 +1,8 @@
+export const Tabs = () =>{
+    return(
+        <div className="flex items-center gap-7 mt-7 px-4 font-semibold">
+            <button className="bg-white px-3 py-2 rounded active:scale-95 ">Photos</button>
+            <button className="bg-white px-3 py-2 rounded active:scale-95 ">Videos</button>
+        </div>
+    )
+}
