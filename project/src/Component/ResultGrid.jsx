348ac79dcd6ae413f86a1a8acc600d8149bb78ae
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2026-04-14 18:28:23 +0530

    fetching data through tabs

--- project/src/Component/ResultGrid.jsx
+++ project/src/Component/ResultGrid.jsx
@@ -1,25 +1,49 @@
+import { useEffect } from 'react';
 import { fetchPhotos, fetchVideo } from '../API/mediaApi'
 import { setQuery, setError, setLoading, setResults } from '../Redux/features/SearchSlice';
 import { useDispatch, useSelector } from "react-redux"
+import { getAdapter } from 'axios';
 
 export const ResultGrid = () => {
 
     const { query, activeTab, result, loading, err } = useSelector((store) => store.search)
 
-    async function getData() {
-        let data = null;
-        if (activeTab === "photos") {
-            data = await fetchPhotos(query)
+    useEffect(() => {
+
+        if(query === "") return
+
+        async function getData() {
+            let data = null;
+            if (activeTab === "photos") {
+                let response = await fetchPhotos(query)
+                data = response.photos.map((item)=>({
+                    id : item.id,
+                    type : "photos",
+                    title : item.alt,
+                    thumbnail : item.src.small,
+                    src : item.url ,
+                }))
+            }
+            if (activeTab === "videos") {
+                let response = await fetchVideo(query)
+
+                data = response.videos.map((item)=>({
+                    id :item.id,
+                    type : "videos",
+                    title : item.user,
+                    thumbnail : item.url.name,
+                    src : item.video_files[0].link
+                }))
+            }
+            console.log(data)
         }
-        if (activeTab === "videos") {
-            data = await fetchVideo(query)
-        }
-        console.log(data)
-    }
+        getData()
+
+    }, [query, activeTab])
+
     return (
         <div>
-            <button className='text-white'
-                onClick={() => getData()}>get data </button>
+            
         </div>
     )
 }
